b32b45caf78b5708b7cc4d461d386698f02955ab
MariusHendriks <Mark.hendriks9999@gmail.com>
2019-07-01 20:49:03 +0200

    Styling chagnes

--- src/components/Band/BandSliderText.js
+++ src/components/Band/BandSliderText.js
@@ -6,6 +6,7 @@ class BandSliderText extends Component {
       <div class="BandSliderText">
         <div class="instrument">{this.props.instrument}</div>
         <div class="name">{this.props.name}</div>
+
         <div class="bio">{this.props.bio}</div>
       </div>
     );
